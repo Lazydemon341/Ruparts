b284bc861cc06ff4bed7eac4cf1da41d88df8cc5
Alexander Vlasyuk <vlas.s.341@gmail.com>
2025-05-07 10:35:01 +0300

    add hilt entrypoint annotation

--- app/src/main/java/com/ruparts/app/features/taskslist/presentation/TasksListFragment.kt
+++ app/src/main/java/com/ruparts/app/features/taskslist/presentation/TasksListFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.ruparts.app.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TasksListFragment : Fragment(){
 
     private val viewModel: TasksListViewModel by viewModels()
