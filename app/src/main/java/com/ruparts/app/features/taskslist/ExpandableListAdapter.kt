1cf5d9c5831d72489e81e45758b38ac43c8ff7dc
Alena <alena.zhukova1506@gmail.com>
2025-05-10 22:15:03 +0500

    getGroupView, getChildView

--- app/src/main/java/com/ruparts/app/features/taskslist/ExpandableListAdapter.kt
+++ app/src/main/java/com/ruparts/app/features/taskslist/ExpandableListAdapter.kt
@@ -10,6 +10,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.ruparts.app.R
 import com.ruparts.app.features.taskslist.model.TaskListGroup
+import com.ruparts.app.features.taskslist.model.TaskListItem
+import com.ruparts.app.features.taskslist.model.TaskPriority
 
 
 class ExpandableListAdapter(private val context: Context) : BaseExpandableListAdapter() {
@@ -30,11 +32,11 @@ class ExpandableListAdapter(private val context: Context) : BaseExpandableListAd
         return items[groupPosition].tasks.size
     }
 
-    override fun getGroup(groupPosition: Int): Any {
+    override fun getGroup(groupPosition: Int): TaskListGroup {
         return items[groupPosition]
     }
 
-    override fun getChild(groupPosition: Int, childPosition: Int): Any {
+    override fun getChild(groupPosition: Int, childPosition: Int): TaskListItem {
         return items[groupPosition].tasks[childPosition]
     }
 
@@ -55,6 +57,7 @@ class ExpandableListAdapter(private val context: Context) : BaseExpandableListAd
         val view = layoutInflater.inflate(R.layout.expandable_list_group, parent, false)
 
         var textView = view.findViewById<TextView>(R.id.exp_list_group_title)
+        textView.text = getGroup(groupPosition).title
 
         return view
     }
@@ -68,11 +71,23 @@ class ExpandableListAdapter(private val context: Context) : BaseExpandableListAd
     ): View {
         val view = layoutInflater.inflate(R.layout.expandable_list_child, parent, false)
 
-        var priorityImage = view.findViewById<ImageView>(R.id.item_priority)
+        var itemPriorityImage = view.findViewById<ImageView>(R.id.item_priority)
         var itemName = view.findViewById<TextView>(R.id.item_name)
         var itemDate = view.findViewById<TextView>(R.id.item_date)
         var itemNote = view.findViewById<TextView>(R.id.item_note)
         var itemDescription = view.findViewById<TextView>(R.id.item_comment)
+
+        val item: TaskListItem = getChild(groupPosition, childPosition)
+
+        itemName.text = item.title
+        itemDate.text = item.date
+        itemDescription.text = item.description
+
+        when (item.priority) {
+            (TaskPriority.HIGH) -> itemPriorityImage.setImageResource(R.drawable.baseline_keyboard_double_arrow_up_24)
+            (TaskPriority.LOW) -> itemPriorityImage.setImageResource(R.drawable.keyboard_double_arrow_down_24dp_40c8ff_fill0_wght400_grad0_opsz24)
+            else -> itemPriorityImage.setImageResource(R.drawable.equal_24dp_faed00_fill0_wght400_grad0_opsz24)
+        }
 
         return view
     }
