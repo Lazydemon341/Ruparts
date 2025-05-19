437a634290f1449ca2929c53250256edbf0da3c1
Alena <alena.zhukova1506@gmail.com>
2025-05-19 17:14:07 +0500

    task fragment

--- app/src/main/java/com/ruparts/app/features/task/presentation/TaskFragment.kt
+++ app/src/main/java/com/ruparts/app/features/task/presentation/TaskFragment.kt
@@ -7,14 +7,17 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.ruparts.app.R
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.textview.MaterialTextView
+import com.ruparts.app.core.extensions.collectWhileStarted
+import com.ruparts.app.features.task.presentation.model.TaskScreenState
 import com.ruparts.app.features.taskslist.model.TaskListItem
 import com.ruparts.app.features.taskslist.model.TaskPriority
-import com.ruparts.app.features.taskslist.model.TaskStatus
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -31,6 +34,8 @@ class TaskFragment : Fragment() {
     private lateinit var changedDate: TextView
     private lateinit var toolbar: Toolbar
 
+    private lateinit var task : TaskListItem
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -42,12 +47,32 @@ class TaskFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        toolbar = view.findViewById(R.id.task_toolbar)
-//        toolbar.setNavigationOnClickListener {
-//            findNavController().navigate(R.id.action_taskFragment_to_taskslistFragment)
-//        }
+        toolbar = view.findViewById(R.id.task_toolbar)
+        toolbar.setNavigationOnClickListener {
+            findNavController().navigate(R.id.action_taskFragment_to_taskslistFragment)
+        }
 
-//        (activity as? AppCompatActivity)?.supportActionBar?.hide()
+        (activity as? AppCompatActivity)?.supportActionBar?.hide()
+
+            description.setText(task.description)
+
+        when (task.priority) { //это
+            TaskPriority.HIGH -> {
+                priorityImage.setImageResource(R.drawable.arrow_up)
+                priority.text = "Высокий"
+            }
+
+            TaskPriority.LOW -> {
+                priorityImage.setImageResource(R.drawable.arrow_down)
+                priority.text = "Низкий"
+            }
+
+            TaskPriority.MEDIUM -> {
+                priorityImage.setImageResource(R.drawable.equal)
+                priority.text = "Средний"
+            }
+        }
+        createdDate.text = task.date
 
         description = view.findViewById(R.id.description_view)
         implementer = view.findViewById(R.id.implementer_view)
@@ -59,7 +84,18 @@ class TaskFragment : Fragment() {
 
 
 
+        observeScreenState()
+    }
 
+
+
+    private fun observeScreenState() { //??
+        viewModel.screenState
+    }
+
+    private fun updateUI(state: TaskScreenState) {
+        task = state.task
+        toolbar.title = state.title
     }
 
 
--- app/src/main/java/com/ruparts/app/features/task/presentation/TaskViewModel.kt
+++ app/src/main/java/com/ruparts/app/features/task/presentation/TaskViewModel.kt
@@ -1,36 +1,18 @@
 package com.ruparts.app.features.task.presentation
 
 import androidx.lifecycle.ViewModel
-import com.ruparts.app.R
+import com.ruparts.app.features.task.presentation.model.TaskScreenState
 import com.ruparts.app.features.taskslist.model.TaskListItem
 import com.ruparts.app.features.taskslist.model.TaskPriority
 import com.ruparts.app.features.taskslist.model.TaskStatus
-import com.ruparts.app.features.taskslist.presentation.mockScreenState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import javax.inject.Inject
 
 @HiltViewModel
 class TaskViewModel @Inject constructor() : ViewModel(){
 
-//    description.text = mockTask.description // пишет, что неверный тип, ждет editable
-//
-//    when (mockTask.priority) { //это
-//        TaskPriority.HIGH -> {
-//            priorityImage.setImageResource(R.drawable.arrow_up)
-//            priority.text = "Высокий"
-//        }
-//        TaskPriority.LOW -> {
-//            priorityImage.setImageResource(R.drawable.arrow_down)
-//            priority.text = "Низкий"
-//        }
-//        TaskPriority.MEDIUM -> {
-//            priorityImage.setImageResource(R.drawable.equal)
-//            priority.text = "Средний"
-//        }
-//    }
-//    createdDate.text = mockTask.date
+    val screenState = mockScreenState //??
 
 }
 
@@ -41,4 +23,9 @@ private val mockTask = TaskListItem(
     title = "Приёмка груза от МаксимумСПБ",
     description = "Номер заказа: 3321\nДоставка: ТК деловые линии",
     date = "10 июн 23"
+)
+
+private val mockScreenState = TaskScreenState(
+    title = "Задача",
+    task = mockTask,
 )
