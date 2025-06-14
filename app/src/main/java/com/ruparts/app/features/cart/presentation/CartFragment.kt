ba3ec4f39fa58a34402d9e5783d608ea05140169
Alena <alena.zhukova1506@gmail.com>
2025-06-14 17:13:10 +0500

    cart page

---
+++ app/src/main/java/com/ruparts/app/features/cart/presentation/CartFragment.kt
@@ -0,0 +1,35 @@
+package com.ruparts.app.features.cart.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.ruparts.app.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CartFragment: Fragment() {
+
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_cart, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textView = view.findViewById(R.id.empty_cart)
+        button = view.findViewById(R.id.add_button)
+
+    }
+
+}
