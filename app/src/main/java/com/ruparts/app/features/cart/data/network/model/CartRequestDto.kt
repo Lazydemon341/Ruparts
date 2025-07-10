66994b65f6a81b1941299f290a9864e0d0b8b4e8
Alena <alena.zhukova1506@gmail.com>
2025-07-10 21:40:54 +0500

    cart item dto

---
+++ app/src/main/java/com/ruparts/app/features/cart/data/network/model/CartRequestDto.kt
@@ -0,0 +1,9 @@
+package com.ruparts.app.features.cart.data.network.model
+
+import com.ruparts.app.core.data.network.EndpointRequestDto
+import com.ruparts.app.features.cart.model.CartListItem
+
+class CartRequestDto() : EndpointRequestDto<List<CartListItem>>(
+    action = "mobile.product.common.basket_items",
+    data = null,
+)
