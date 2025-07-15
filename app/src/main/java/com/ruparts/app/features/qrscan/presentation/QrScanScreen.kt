324c4232efc54ab9d6728dd95981781a242c07da
Alena <alena.zhukova1506@gmail.com>
2025-07-15 21:06:16 +0500

    text style

--- app/src/main/java/com/ruparts/app/features/qrscan/presentation/QrScanScreen.kt
+++ app/src/main/java/com/ruparts/app/features/qrscan/presentation/QrScanScreen.kt
@@ -80,10 +80,14 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -475,8 +479,13 @@ private fun QrScanListItem(
                     )
                     Spacer(Modifier.width(5.dp))
                     Text(
-                        text = item.barcode,
-                        fontSize = 14.sp,
+                        buildAnnotatedString {
+                            append(item.barcode.substring(0, item.barcode.length-3))
+                            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color.Black)) {
+                                append(item.barcode.substring(item.barcode.length-3))
+                            }
+                        },
+                        fontSize = 14.sp
                     )
                 }
                 Row(
