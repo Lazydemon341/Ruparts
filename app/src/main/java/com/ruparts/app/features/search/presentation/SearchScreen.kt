73a163293e53fb6f9fd05e40368bd1f014491814
Alena <alena.zhukova1506@gmail.com>
2025-07-30 18:17:14 +0500

    ModalBottomSheet

--- app/src/main/java/com/ruparts/app/features/search/presentation/SearchScreen.kt
+++ app/src/main/java/com/ruparts/app/features/search/presentation/SearchScreen.kt
@@ -3,6 +3,7 @@ package com.ruparts.app.features.search.presentation
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,33 +22,41 @@ import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
+import androidx.navigation.fragment.findNavController
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDownward
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ModalBottomSheetDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.fragment.findNavController
 import com.ruparts.app.R
 import com.ruparts.app.core.ui.components.RupartsCartItem
 import com.ruparts.app.features.cart.model.CartListItem
@@ -269,8 +278,9 @@ private fun SearchScreenFilterDialog(
 ) {
     when (filterType) {
         SearchScreenFilterType.FLAGS -> {
-            // TODO: ModalBottomSheet(...)
+            SearchScreenModalBottomSheet()
         }
+
 
         SearchScreenFilterType.LOCATION -> {
 
@@ -281,6 +291,7 @@ private fun SearchScreenFilterDialog(
         }
     }
 }
+
 
 @Preview
 @Composable
@@ -296,3 +307,116 @@ private fun SearchScreenPreview() {
         )
     )
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SearchScreenModalBottomSheet() {
+    val bottomSheetState = rememberModalBottomSheetState(true)
+    var checked by remember { mutableStateOf(true) }
+    val items = listOf("Требуется измерить", "Требуется взвесить", "Требуется фото")
+//    var checkedItems = mutableStateListOf(items)
+
+    ModalBottomSheet(
+        dragHandle = {BottomSheetDefaults.DragHandle()},
+        sheetState = bottomSheetState,
+        onDismissRequest = {},
+        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+    ) {
+        Box(contentAlignment = Alignment.BottomCenter) {
+            Column {
+                LazyColumn {
+                    stickyHeader(contentType = "listHeader") {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 4.dp, bottom = 12.dp)
+                                .padding(horizontal = 16.dp),
+                            color = MaterialTheme.colorScheme.secondary,
+                            text = "Признаки",
+                            style = MaterialTheme.typography.bodyMedium,
+                            textAlign = TextAlign.Left
+                        )
+                    }
+                }
+                items.forEachIndexed { index, item ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                    ) {
+                        Text(text = item)
+                        Spacer(modifier = Modifier.weight(1f))
+                        Checkbox(
+                            checked = checked,
+                            onCheckedChange = { checked = it }
+                        )
+                    }
+                }
+                if (!checked) {
+                    Button(
+                        onClick = { },
+                        modifier = Modifier
+                            .padding(horizontal = 20.dp)
+                            .padding(top = 8.dp, bottom = 24.dp)
+                            .fillMaxWidth()
+                            .height(56.dp),
+                        shape = RoundedCornerShape(16.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.primary
+                        )
+                    ) {
+                        Text(
+                            text = "Применить",
+                            fontSize = 16.sp,
+                            color = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                } else {
+                    Row(
+                        Modifier.fillMaxWidth()
+                    ) {
+                        Button(
+                            onClick = { },
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(start = 20.dp)
+                                .padding(top = 8.dp, bottom = 24.dp)
+                                .height(56.dp),
+                            shape = RoundedCornerShape(16.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = MaterialTheme.colorScheme.surfaceContainer
+                            )
+                        ) {
+                            Text(
+                                text = "Очистить",
+                                fontSize = 16.sp,
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                        }
+                        Button(
+                            onClick = { },
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(start = 16.dp, end = 20.dp)
+                                .padding(top = 8.dp, bottom = 24.dp)
+                                .height(56.dp),
+                            shape = RoundedCornerShape(16.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = MaterialTheme.colorScheme.primary
+                            )
+                        ) {
+                            Text(
+                                text = "Применить",
+                                fontSize = 16.sp,
+                                color = MaterialTheme.colorScheme.onPrimary
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
