11e31acc6645393bbd078fdb71f003082158ad45
Alexander Vlasyuk <vlas.s.341@gmail.com>
2025-07-30 11:57:34 +0300

    fix search screen layout

--- app/src/main/java/com/ruparts/app/features/search/presentation/SearchScreen.kt
+++ app/src/main/java/com/ruparts/app/features/search/presentation/SearchScreen.kt
@@ -1,5 +1,6 @@
 package com.ruparts.app.features.search.presentation
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
@@ -9,12 +10,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -24,6 +26,8 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDownward
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FilterChip
@@ -35,13 +39,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -50,18 +49,12 @@ import androidx.compose.ui.unit.sp
 import com.ruparts.app.R
 import com.ruparts.app.core.ui.components.RupartsCartItem
 import com.ruparts.app.features.cart.model.CartListItem
-import com.ruparts.app.features.cart.model.CartScanPurpose
-import com.ruparts.app.features.qrscan.presentation.model.QrScanScreenState
-
-private const val listItemContentType = "listItem"
 
 @Composable
 fun SearchScreen(
     state: SearchScreenState
 ) {
-
     val scrollState = rememberScrollState()
-
     Scaffold(
         contentWindowInsets = WindowInsets.systemBars.only(
             WindowInsetsSides.Horizontal + WindowInsetsSides.Bottom
@@ -80,132 +73,163 @@ fun SearchScreen(
             }
         },
         bottomBar = {
-            Box {
-            Surface(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .fillMaxWidth()
-                    .padding(
-                        WindowInsets.systemBars.only(
-                            WindowInsetsSides.Horizontal + WindowInsetsSides.Bottom
-                        ).asPaddingValues()
-                    ),
-                color = MaterialTheme.colorScheme.surface,
-                shadowElevation = 8.dp,
-            ) {
-                Button(
-                    onClick = { },
-                    modifier = Modifier
-                        .padding(horizontal = 20.dp)
-                        .padding(top = 8.dp, bottom = 24.dp)
-                        .fillMaxWidth()
-                        .height(56.dp),
-                    shape = RoundedCornerShape(16.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = MaterialTheme.colorScheme.secondaryContainer
-                    )
-                ) {
-                    Text(
-                        text = "Сборка",
-                        fontSize = 16.sp,
-                        color = MaterialTheme.colorScheme.onSecondaryContainer
-                    )
-                }
-            }
-            }
-        }
+            SearchScreenAssemblyButton()
+        },
+        containerColor = MaterialTheme.colorScheme.surfaceContainer,
     ) { paddingValues ->
-        Column(Modifier.padding(paddingValues)) {
-            Column(
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+        ) {
+            SearchScreenFilters(
+                filters = state.filters,
+                scrollState = scrollState,
+            )
+            SearchScreenSorting()
+            SearchScreenItems(
+                items = state.items,
+            )
+        }
+    }
+}
+
+@Composable
+private fun SearchScreenFilters(
+    filters: List<SearchScreenFilter>,
+    scrollState: ScrollState = rememberScrollState()
+) {
+    Row(
+        modifier = Modifier
+            .padding(vertical = 12.dp)
+            .horizontalScroll(scrollState)
+            .wrapContentHeight(),
+    ) {
+        Spacer(modifier = Modifier.width(16.dp))
+        filters.forEachIndexed { index, filter ->
+            FilterChip(
                 modifier = Modifier
-                    .background(color = MaterialTheme.colorScheme.surfaceContainer)
-            ) {
-
-                Row(
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp, vertical = 12.dp)
-                        .background(color = MaterialTheme.colorScheme.surfaceContainer)
-                        .horizontalScroll(scrollState)
-                        .wrapContentHeight(),
-                ) {
-
-                    state.filters.forEach { filter ->
-                        FilterChip(
-                            modifier = Modifier
-                                .border(
-                                    width = 1.dp,
-                                    color = MaterialTheme.colorScheme.outlineVariant,
-                                    shape = RoundedCornerShape(8.dp)
-                                )
-                                .wrapContentHeight()
-                                .height(32.dp),
-                            colors = FilterChipDefaults.filterChipColors(
-                                containerColor = MaterialTheme.colorScheme.surfaceContainer,
-                                selectedContainerColor = MaterialTheme.colorScheme.secondaryContainer
-                            ),
-                            onClick = { },
-                            label = { Text(
-                                text = filter.text,
-                                color = MaterialTheme.colorScheme.onSecondaryContainer) },
-                            selected = filter.selected,
-                            trailingIcon = {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.trailing_icon),
-                                    contentDescription = "",
-                                )
-                            },
-                        )
-                        Spacer(modifier = Modifier.width(12.dp))
-                    }
-                }
-
-                Row(
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp, vertical = 12.dp)
-                        .background(color = MaterialTheme.colorScheme.surfaceContainer),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Icon(imageVector = Icons.Default.ArrowDownward, contentDescription = null)
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(
-                        textAlign = TextAlign.Center,
-                        text = "По количеству",
-                        fontSize = 14.sp,
-                        color = MaterialTheme.colorScheme.onSecondaryContainer
+                    .border(
+                        width = 1.dp,
+                        color = MaterialTheme.colorScheme.outlineVariant,
+                        shape = RoundedCornerShape(8.dp)
                     )
-                }
-            }
-
-
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(color = MaterialTheme.colorScheme.surfaceContainer),
-            ) {
-                LazyColumn(
-                    modifier = Modifier.fillMaxSize(),
-                ) {
-                    itemsIndexed(
-                        items = state.items,
-                        key = { _, item -> item.id },
-                        contentType = { _, _ -> listItemContentType },
-                    ) { index, item ->
-                        RupartsCartItem(
-                            item = item,
-                            onRemove = {},
-                            enableSwipeToDismiss = false,
-                            isRowVisible = true
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                    }
-
-                }
-
+                    .wrapContentHeight()
+                    .height(32.dp),
+                colors = FilterChipDefaults.filterChipColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                    labelColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    selectedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                    selectedLabelColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                ),
+                onClick = { },
+                label = {
+                    Text(text = filter.text)
+                },
+                selected = filter.selected,
+                trailingIcon = {
+                    Icon(
+                        modifier = Modifier.size(18.dp),
+                        imageVector = if (filter.selected) {
+                            Icons.Default.Close
+                        } else {
+                            Icons.Default.ArrowDropDown
+                        },
+                        contentDescription = "",
+                    )
+                },
+            )
+            if (index == filters.lastIndex) {
+                Spacer(modifier = Modifier.width(16.dp))
+            } else {
+                Spacer(modifier = Modifier.width(12.dp))
             }
         }
     }
 }
 
+@Composable
+private fun SearchScreenSorting() {
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 12.dp)
+            .background(color = MaterialTheme.colorScheme.surfaceContainer),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            modifier = Modifier.size(20.dp),
+            imageVector = Icons.Default.ArrowDownward,
+            contentDescription = null,
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            textAlign = TextAlign.Center,
+            text = "По количеству",
+            fontSize = 14.sp,
+            color = MaterialTheme.colorScheme.onSecondaryContainer
+        )
+    }
+}
+
+private const val listItemContentType = "listItem"
+
+@Composable
+private fun SearchScreenItems(
+    items: List<CartListItem>
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.surfaceContainer),
+    ) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            itemsIndexed(
+                items = items,
+                key = { _, item -> item.id },
+                contentType = { _, _ -> listItemContentType },
+            ) { index, item ->
+                RupartsCartItem(
+                    item = item,
+                    onRemove = {},
+                    enableSwipeToDismiss = false,
+                    isRowVisible = true
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
+    }
+}
+
+@Composable
+private fun SearchScreenAssemblyButton() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .navigationBarsPadding(),
+        color = MaterialTheme.colorScheme.surface,
+        shadowElevation = 8.dp,
+    ) {
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .padding(top = 8.dp, bottom = 24.dp)
+                .fillMaxWidth()
+                .height(56.dp),
+            shape = RoundedCornerShape(16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.secondaryContainer
+            )
+        ) {
+            Text(
+                text = "Сборка",
+                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.onSecondaryContainer
+            )
+        }
+    }
+}
 
 @Preview
 @Composable
@@ -214,7 +238,7 @@ private fun SearchScreenPreview() {
         state = SearchScreenState(
             items = emptyList(),
             filters = listOf(
-                SearchScreenFilter("Признаки", false),
+                SearchScreenFilter("Признаки", true),
                 SearchScreenFilter("Расположение", false),
                 SearchScreenFilter("Выборки", false)
             )
--- app/src/main/java/com/ruparts/app/features/search/presentation/SearchScreenState.kt
+++ app/src/main/java/com/ruparts/app/features/search/presentation/SearchScreenState.kt
@@ -1,13 +1,15 @@
 package com.ruparts.app.features.search.presentation
 
+import androidx.compose.runtime.Immutable
 import com.ruparts.app.features.cart.model.CartListItem
 
-class SearchScreenState(
+@Immutable
+data class SearchScreenState(
     val items: List<CartListItem>,
-    val filters: List<SearchScreenFilter>
+    val filters: List<SearchScreenFilter>,
 )
 
-class SearchScreenFilter(
+data class SearchScreenFilter(
     val text: String,
-    val selected: Boolean
+    val selected: Boolean,
 )
