84c2b5eb17d9111b2aac152105b8a9c787674411
Alena <alena.zhukova1506@gmail.com>
2025-08-17 20:36:28 +0500

    add to cart bottom sheet 2

--- app/src/main/java/com/ruparts/app/features/assembly/presentation/AssemblyAddToCartBottomSheet.kt
+++ app/src/main/java/com/ruparts/app/features/assembly/presentation/AssemblyAddToCartBottomSheet.kt
@@ -2,18 +2,26 @@ package com.ruparts.app.features.assembly.presentation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -53,7 +62,7 @@ fun AssemblyAddToCartBottomSheet(
         ) {
             Column() {
                 Text(
-                    modifier = Modifier.Companion
+                    modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 16.dp, vertical = 8.dp),
                     color = colorResource(R.color.neutral60),
@@ -61,45 +70,105 @@ fun AssemblyAddToCartBottomSheet(
                     style = MaterialTheme.typography.bodyMedium,
                     textAlign = TextAlign.Center,
                 )
-                Column(
+                Box(
                     modifier = Modifier
-                        .background(MaterialTheme.colorScheme.surface, shape = RoundedCornerShape(100.dp))
-                        .padding(vertical = 12.dp, horizontal = 16.dp),
+                        .padding(horizontal = 16.dp, vertical = 12.dp),
                 ) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
+                    Column(
+                        modifier = Modifier
+                            .background(MaterialTheme.colorScheme.surface, shape = RoundedCornerShape(12.dp))
+                            .padding(horizontal = 16.dp, vertical = 12.dp),
                     ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = item.article,
+                                color = MaterialTheme.colorScheme.onSurface,
+                                style = TextStyle(fontWeight = FontWeight.Bold),
+                                fontSize = 16.sp
+                            )
+                            Text(
+                                text = item.quantity.toString(),
+                                color = MaterialTheme.colorScheme.onSurface,
+                                fontSize = 14.sp,
+                                modifier = Modifier
+                                    .border(1.dp, SolidColor(Color.Black), RoundedCornerShape(percent = 20))
+                                    .padding(horizontal = 6.dp, vertical = 1.dp)
+                            )
+                        }
                         Text(
-                            text = item.article,
-                            color = MaterialTheme.colorScheme.onSurface,
-                            style = TextStyle(fontWeight = FontWeight.Bold),
-                            fontSize = 16.sp
-                        )
-                        Text(
-                            text = item.quantity.toString(),
+                            text = item.brand,
                             color = MaterialTheme.colorScheme.onSurface,
                             fontSize = 14.sp,
-                            modifier = Modifier
-                                .border(1.dp, SolidColor(Color.Black), RoundedCornerShape(percent = 20))
-                                .padding(horizontal = 6.dp, vertical = 1.dp)
+                            style = MaterialTheme.typography.bodyMedium,
+                        )
+                        Text(
+                            text = item.description,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            fontSize = 14.sp,
+                            style = MaterialTheme.typography.bodyMedium,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
                         )
                     }
-                    Text(
-                        text = item.brand,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        fontSize = 14.sp,
-                        style = MaterialTheme.typography.bodyMedium,
-                    )
-                    Text(
-                        text = item.description,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        fontSize = 14.sp,
-                        style = MaterialTheme.typography.bodyMedium,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
-                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    modifier = Modifier
+                        .padding(start = 20.dp, top = 8.dp, bottom = 48.dp, end = 20.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Button(
+                        onClick = {},
+                        shape = RoundedCornerShape(16.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.secondaryContainer
+                        ),
+                        modifier = Modifier.height(56.dp),
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = null,
+                            modifier = Modifier.size(24.dp),
+                            tint = MaterialTheme.colorScheme.onSecondaryContainer,
+                        )
+                        Text(
+                            modifier = Modifier.padding(start = 8.dp),
+                            text = "Отменить",
+                            fontSize = 16.sp,
+                            color = MaterialTheme.colorScheme.onSecondaryContainer
+                        )
+                    }
+                    IconButton(
+                        onClick = {},
+                        modifier = Modifier
+                            .size(56.dp)
+                            .background(MaterialTheme.colorScheme.secondaryContainer, RoundedCornerShape(16.dp))
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.split),
+                            contentDescription = null,
+                            modifier = Modifier.size(24.dp),
+                            tint = MaterialTheme.colorScheme.onSecondaryContainer,
+                        )
+                    }
+                    IconButton(
+                        onClick = {},
+                        modifier = Modifier
+                            .size(56.dp)
+                            .background(MaterialTheme.colorScheme.secondaryContainer, RoundedCornerShape(16.dp))
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.alert),
+                            contentDescription = null,
+                            modifier = Modifier.size(24.dp),
+                            tint = MaterialTheme.colorScheme.onSecondaryContainer,
+                        )
+                    }
                 }
             }
         }
